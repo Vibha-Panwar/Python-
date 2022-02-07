befffce26298c402c8acb9e99a2d1c8716538deb
Vibha-Panwar <vibha9774@gmail.com>
2022-02-07 09:44:32 +0530

    7-feb-2022 1st program

---
+++ 001.twinkletwinkle.py
@@ -0,0 +1,10 @@
+# https://www.w3resource.com/python-exercises/python-basic-exercise-1.php
+
+###
+# Write a Python program to print the following string in a specific format (see the output).
+
+# Sample String: "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high,
+#  Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are!"
+
+print("Twinkle, twinkle, little star,\n\tHow I wonder what you are!\n\t\tUp above the world so high,")
+print("\t\tLike a diamond in the sky.\nTwinkle twinkle little star,\n\tHow I wonder what you are!")
