1e23e8f61a3842bbb359c948cbf1d0b87455392b
Vibha-Panwar <vibha9774@gmail.com>
2022-02-09 21:36:05 +0530

    9 feb 2022

---
+++ 002.pythonversion.py
@@ -0,0 +1,10 @@
+# https://www.w3resource.com/python-exercises/python-basic-exercise-2.php
+
+# Write a Python program to get the Python version you are using.
+
+import sys
+
+print("python version")
+print(sys.version)
+print("version info")
+print(sys.version_info)
