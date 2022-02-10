715743a58bd298caf97c21fcf0bf95de6a97cfda
Vibha-Panwar <vibha9774@gmail.com>
2022-02-10 07:04:24 +0530

    10-feb-2022

---
+++ 003.DateTime.py
@@ -0,0 +1,8 @@
+# https://www.w3resource.com/python-exercises/python-basic-exercise-3.php
+
+# Write a Python program to display the current date and time.
+
+import datetime
+
+now=datetime.datetime.now()
+print("current date and time: ", now)
