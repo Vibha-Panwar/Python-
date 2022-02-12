be9127b90b79950b6a165e2137c22247ecc054e1
Vibha-Panwar <vibha9774@gmail.com>
2022-02-12 11:21:58 +0530

    12 feb 2022

---
+++ 005.firstandlastname.py
@@ -0,0 +1,7 @@
+# https://www.w3resource.com/python-exercises/python-basic-exercise-5.php
+
+# Write a Python program which accepts the user's first and last name and print them in reverse order with a space between them
+
+fname = input("input your first name :")
+lname = input("input your last name:")
+print("hello  " +  lname  +  fname)
