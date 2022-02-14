3b836fef4065472147280856cb9cddea5b30d5a0
Vibha-Panwar <vibha9774@gmail.com>
2022-02-14 07:25:36 +0530

    14-feb-2022

---
+++ 007.colourlist.py
@@ -0,0 +1,7 @@
+# https://www.w3resource.com/python-exercises/python-basic-exercise-8.php
+
+# Write a Python program to display the first and last colors from the following list.
+# color_list = ["Red","Green","White" ,"Black"]
+
+color_list = ["red","green","white","black"]
+print("%s%s"%(color_list[0],color_list[-1]))
