6929688dafcb049ce3cd834c0973531c4d84fdf9
Vibha-Panwar <vibha9774@gmail.com>
2022-02-14 07:33:57 +0530

    modified color list 14-feb-2022

--- 007.colourlist.py
+++ 007.colourlist.py
@@ -3,5 +3,8 @@
 # Write a Python program to display the first and last colors from the following list.
 # color_list = ["Red","Green","White" ,"Black"]
 
-color_list = ["red","green","white","black"]
-print("%s%s"%(color_list[0],color_list[-1]))
+input_list = input("enter a list element seprated by comma\t")
+color_list = input_list.split(",")
+print("your color_list is :\t", color_list)
+print("first color :",color_list[0])
+print("last color :",color_list[-1])
