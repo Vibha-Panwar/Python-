ee0a0bc60d904c24db1fb2e8d9d9e429d6487514
Vibha-Panwar <vibha9774@gmail.com>
2022-02-19 07:58:37 +0530

    19-feb-2022 got error in it

---
+++ 013.Quotations.print.py
@@ -0,0 +1,16 @@
+# https://www.w3resource.com/python-exercises/python-basic-exercise-13.php
+
+ #Write a Python program to print the following 'here document'.
+
+ # Sample string:
+ # a string that you "don't" have to escape
+ # This
+ # is a ....... multi-line
+ # heredoc string --------> example
+
+ print ("""
+ a string that you "don't" have to escape
+ This
+ is a .......multi-line
+ herodoc string---------> example
+ """)
