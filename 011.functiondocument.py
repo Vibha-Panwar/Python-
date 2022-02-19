5e583e973fb99ec660434a57940e7800f35e35a3
Vibha-Panwar <vibha9774@gmail.com>
2022-02-19 17:17:03 +0530

    19-feb-2022 remaining one

---
+++ 011.functiondocument.py
@@ -0,0 +1,6 @@
+# https://www.w3resource.com/python-exercises/python-basic-exercise-11.php
+
+# Write a Python program to print the documents (syntax, description etc.) of Python built-in function(s).
+
+print(type.__doc__)
+print(abs.__doc__)
