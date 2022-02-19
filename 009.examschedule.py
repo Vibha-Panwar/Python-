e8f57ca797b5d7df7c57689fd7ad2bd2d2ab3e62
Vibha-Panwar <vibha9774@gmail.com>
2022-02-19 17:30:49 +0530

    19-feb-2022 detailed find out

--- 009.examschedule.py
+++ 009.examschedule.py
@@ -3,7 +3,10 @@
 # Write a Python program to display the examination schedule. (extract the date from exam_st_date).
 
 input_list = input("enter date in DD/MM/YYYY format :")
+print(type(input_list))
 date = input_list.split("/")
+print(type(date))
 print("day:",date[0])
+print(type(date[0]))
 print("month:",date[1])
 print("year:",date[2])
