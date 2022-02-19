5c8e5c00832d13e9cb8990dbaadba20814806de2
Vibha-Panwar <vibha9774@gmail.com>
2022-02-19 17:09:44 +0530

    19-feb-2022 modified

--- 013.Quotations.print.py
+++ 013.Quotations.print.py
@@ -8,9 +8,10 @@
  # is a ....... multi-line
  # heredoc string --------> example
 
- print ("""
- a string that you "don't" have to escape
- This
- is a .......multi-line
- herodoc string---------> example
- """)
+print(
+     """
+     a string that you "don't" have to escape
+     This is a .......multi-line
+     herodoc string---------> example
+     """
+    )
