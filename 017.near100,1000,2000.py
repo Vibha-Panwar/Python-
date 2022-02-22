17b953afbd0929227b9157b40f160d695feb82bb
Vibha-Panwar <vibha9774@gmail.com>
2022-02-22 06:41:41 +0530

    22-feb-2022 (2nd programe)

--- 017.near100,1000,2000.py
+++ 017.near100,1000,2000.py
@@ -1,11 +1,11 @@
 # https://www.w3resource.com/python-exercises/python-basic-exercise-17.php
 
-#Write a Python program to test whether a number is within 100 of 1000 or 2000.
+# Write a Python program to test whether a number is within 100 of 1000 or 2000.
+
+# abs() function returns the absolute value.
 
 def near_thousand(n):
-    return((abs(1000-n)<=100) or (abs(2000-n)<=100))
-print(near_thousand(1000))
-print(near_thousand(900))
-print(near_thousand(800))
-print(near_thousand(2100))
-print(near_thousand(2200))
+    return((abs(1000-n)<=100) or(abs(2000-n)<=100))
+
+n = int(input("Enter your digit :\n"))
+print("Is your digit near to 1000 or 2000 :",near_thousand(n))
