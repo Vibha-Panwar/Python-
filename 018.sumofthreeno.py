ec4b4794f461e8b4438a25f944c38862a52ded09
Vibha-Panwar <vibha9774@gmail.com>
2022-02-23 07:12:50 +0530

    23-feb-2022 {2nd try}

--- 018.sumofthreeno.py
+++ 018.sumofthreeno.py
@@ -1,13 +1,15 @@
 # https://www.w3resource.com/python-exercises/python-basic-exercise-18.php
 
 # Write a Python program to calculate the sum of three given numbers,
-#  if the values are equal then return thrice of their sum.
+# if the values are equal then return thrice of their sum.
 
-def sum_thrice(x,y,z):
-    sum = x+y+z
-    if x == y == z :
-        sum = sum * 3
-    return sum
+n1 = int(input("Enter first digit :\t"))
+n2 = int(input("Enter second digit :\t"))
+n3 = int(input("Enter third digit :\t"))
 
-print(sum_thrice(1,2,3))
-print(sum_thrice(5,5,5))
+if n1==n2==n3:
+    r = n1*3*3
+    print(f"sum of {n1} {n2} {n3} digit is :{r}")
+else:
+    r = n1+n2+n3
+    print(f"sum of {n1} {n2} {n3} digit is : {r}")
