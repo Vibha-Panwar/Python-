8ecadef6cf8ff900c347877d21c0a058b2b2d684
Vibha-Panwar <vibha9774@gmail.com>
2022-02-24 18:25:48 +0530

    24-feb-2022{ 2nd form of program}

--- 021.odd_even.py
+++ 021.odd_even.py
@@ -3,9 +3,9 @@
 # Write a Python program to find whether a given number (accept from the user) is even or odd,
 # print out an appropriate message to the user.
 
-num = int(input("Enter a number :"))
-mod = num % 2
-if mod > 0 :
-    print("This num is an odd number.")
-else :
-    print("This num is an even number.")
+No = int(input("Enter a no.: \t"))
+mod = No % 2
+if mod == 0:
+    print("",No,"is an even number.")
+else:
+    print("",No,"is an odd number.")
