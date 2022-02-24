54874c6112deec68cd5e3667c4326b8b0d80407f
Vibha-Panwar <vibha9774@gmail.com>
2022-02-24 06:34:30 +0530

    24-feb-2022 [2nd]

--- 019.Is_string.py
+++ 019.Is_string.py
@@ -2,10 +2,10 @@
 # Write a Python program to get a new string from a given string where "Is" has been added to the front. 
 # If the given string already begins with "Is" then return the string unchanged.
 
-def new_string(str):
+def V1(str):
     if len(str) >= 2 and str[:2] == "Is":
         return str
     return "Is" + str
 
-print(new_string("array"))
-print(new_string("IsEmpty"))
+print(V1("array"))
+print(V1("IsEmpty"))
