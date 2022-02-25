634838ba763be0c509986dc631d94441721fce84
Vibha-Panwar <vibha9774@gmail.com>
2022-02-25 07:17:52 +0530

    25-feb-2022{2nd format}

--- 023.n_copyofgivenstring.py
+++ 023.n_copyofgivenstring.py
@@ -1,17 +1,20 @@
 # https://www.w3resource.com/python-exercises/python-basic-exercise-23.php
 
-# Write a Python program to get the n (non-negative integer) copies of the first 2 characters of a given string.
+# Write a Python program to get the n (non-negative integer) copies of the first 2 characters of a given string. 
 # Return the n copies of the whole string if the length is less than 2
 
+string = input("Enter your string:")
+n = int(input("No. of copies :"))
+
 def substring_copy(str,n):
-    flen =2
-    if flen >len(str):
+    flen = 2
+    if flen > len(str):
         flen = len(str)
     substr = str[:flen]
-
     result = ""
     for i in range(n):
         result = result + substr
+
     return result
-print(substring_copy('.abcdef',2))
-print(substring_copy('*v',7))
+
+print(substring_copy(string , n))   
