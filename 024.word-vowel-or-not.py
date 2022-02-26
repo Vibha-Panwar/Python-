288646d4f01c4b0592225fe11d281bf2ec70949a
Vibha-Panwar <vibha9774@gmail.com>
2022-02-26 08:28:52 +0530

    26-feb-2022 {alittlechange}

--- 024.word-vowel-or-not.py
+++ 024.word-vowel-or-not.py
@@ -2,8 +2,8 @@
 
 # Write a Python program to test whether a passed letter is a vowel or not
 
-def is_vowel(char):
+def w_vowel(char):
     all_vowels = 'aeiou'
     return char in all_vowels
-print(is_vowel('c'))
-print(is_vowel('e'))
+print(w_vowel('c'))
+print(w_vowel('e'))
