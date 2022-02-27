e4aef3cfe8cbe71e4c3c971fd2118fc4625e109e
Vibha-Panwar <vibha9774@gmail.com>
2022-02-27 11:59:59 +0530

    27-feb-2022

--- 020.repeatstring.py
+++ 020.repeatstring.py
@@ -5,7 +5,11 @@
 string = input("Enter your string :\t")
 times = int(input("How many times you want to repeat : \t"))
 result = ""
-for i in range(times):
+print(type(result))
+for count in range(times):
     result = result + string
+    print(type(count))
+    print("\n count value",count)
+    print("string as of now \n", result)
 
 print("New string : \n",result)
