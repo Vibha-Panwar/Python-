b58c4c7c5ad55e1cc985ab1802121ba062a1fb8c
Vibha-Panwar <vibha9774@gmail.com>
2022-03-04 07:32:39 +0530

    4-march-2022

---
+++ 037.personal.details.py
@@ -0,0 +1,9 @@
+# https://www.w3resource.com/python-exercises/python-basic-exercise-37.php
+# Write a Python program to display your details like name, age, address in three different lines.
+
+def Personal_Details():
+    Name,Age = "Vibha" , 25
+    Address = "Muzaffarnagar, India "
+    print("Name:{}\n Age:{}\n Address:{}".format(Name,Age,Address))
+
+Personal_Details ()
