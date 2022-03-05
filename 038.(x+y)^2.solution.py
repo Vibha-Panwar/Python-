00e1e95a38a10f0dabd528531881ef22b97d57dc
Vibha-Panwar <vibha9774@gmail.com>
2022-03-05 06:38:13 +0530

    5-march-2022

---
+++ 038.(x+y)^2.solution.py
@@ -0,0 +1,6 @@
+# https://www.w3resource.com/python-exercises/python-basic-exercise-38.php
+# Write a Python program to solve (x + y) * (x + y).
+
+x,y = 5,9
+result = x*x + 2*x*y + y*y
+print("({} + {})^2) = {}" . format(x,y,result))
