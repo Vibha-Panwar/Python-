8e1d30d03aae2001df6aecd34dfa9ffdd99932f9
Vibha-Panwar <vibha9774@gmail.com>
2022-03-06 07:21:51 +0530

    6-march-2022{ with a little detail}

--- 042.python.32bit.or.64bit.py
+++ 042.python.32bit.or.64bit.py
@@ -1,5 +1,6 @@
 # https://www.w3resource.com/python-exercises/python-basic-exercise-42.php
 # Write a Python program to determine if a Python shell is executing in 32bit or 64bit mode on OS.
 
-import struct
-print (struct.calcsize("P")*8)
+import platform, struct
+print(platform.architecture()[0])
+print(struct.calcsize("P")*8)
