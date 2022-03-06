7ea1499322b5434c56cdf0f44763cc6c3b107089
Vibha-Panwar <vibha9774@gmail.com>
2022-03-06 12:10:56 +0530

    6-march-2022{got error in it}

--- 045.external.command.py
+++ 045.external.command.py
@@ -1,5 +1,5 @@
 # https://www.w3resource.com/python-exercises/python-basic-exercise-45.php
 # Write a python program to call an external command in Python.
 
-from subprocess import call
-call(["ls","-l"])
+import os
+print(os.system('ls -l'))
