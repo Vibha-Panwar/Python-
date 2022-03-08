3e3b4935355db6fb895ca27ce30111eea5850456
Vibha-Panwar <vibha9774@gmail.com>
2022-03-08 08:03:58 +0530

    8-march-2022

---
+++ 1.learn.python.hardway/003(i).Numbers.And.Math.py
@@ -0,0 +1,9 @@
+# book said to do math program again on our own.
+# so I am trying to do it.
+
+print("I will now count the years :")
+
+print("Starting Year",1990)
+print("Current Year",2022)
+
+print("Year Gap",2022-1990)
