5897e6a0087e4d91b843b5e0f5ebbcc22ec020ac
Vibha-Panwar <vibha9774@gmail.com>
2022-03-10 17:57:19 +0530

    10-march-2022

---
+++ 1.learn.python.hardway/005(i).practice.py
@@ -0,0 +1,8 @@
+height = 1.75 # meters
+weight = 75 # kg
+
+change_height = height * 100
+change_weight = weight * 2.2
+
+print(f"Now the converted height is {change_height} .")
+print(f"Now the converted weight is {change_weight} .")
