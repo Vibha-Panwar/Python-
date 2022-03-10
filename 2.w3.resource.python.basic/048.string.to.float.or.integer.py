1b8a5d0800d9ab3b5a07f78f3f9a78058d408f92
Vibha-Panwar <vibha9774@gmail.com>
2022-03-10 18:26:15 +0530

    10-march-2022

---
+++ 2.w3.resource.python.basic/048.string.to.float.or.integer.py
@@ -0,0 +1,6 @@
+# https://www.w3resource.com/python-exercises/python-basic-exercise-48.php
+# Write a Python program to parse a string to Float or Integer.
+
+n = 246.2524
+print(float(n))
+print(int(float(n)))
