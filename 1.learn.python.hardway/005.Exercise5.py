2d508bc730fed7f74c8ec8eb5f6706e23a47b2f9
Vibha-Panwar <vibha9774@gmail.com>
2022-03-10 17:46:30 +0530

    10-march-2022

---
+++ 1.learn.python.hardway/005.Exercise5.py
@@ -0,0 +1,18 @@
+
+my_name = 'Vibha Panwar'
+my_age = 25
+my_height = 166 # centimeters
+my_weight = 75 # kg
+my_eyes = 'black'
+my_teeth = 'white'
+my_hair = 'black'
+
+print(f"let's talk about {my_name}.")
+print(f"she's {my_height} centimeter tall.")
+print(f"she is {my_weight} kg heavy.")
+print("Actually that is too heavy.")
+print(f"she's got {my_eyes} eyes and {my_hair} hair.")
+print(f"her teeth are usually {my_teeth} depending on the coffee.")
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age},{my_height}, and {my_weight} I get {total}.")
