5b19ee84485bde33234b29adcf5498f2155dc7d5
Vibha-Panwar <vibha9774@gmail.com>
2022-03-10 18:39:22 +0530

    10-march-2022

---
+++ 2.w3.resource.python.basic/050.print.without.space.py
@@ -0,0 +1,6 @@
+# https://www.w3resource.com/python-exercises/python-basic-exercise-50.php
+# Write a Python program to print without newline or space.
+
+for i in range(0,10):
+    print('Will I ever meet BTS.', end="")
+print("\n")
