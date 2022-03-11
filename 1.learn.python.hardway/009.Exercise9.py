d6b94c10d7e03de4e8c258c850eaa6cdf247a2ca
Vibha-Panwar <vibha9774@gmail.com>
2022-03-11 08:58:05 +0530

    11-march-2022

---
+++ 1.learn.python.hardway/009.Exercise9.py
@@ -0,0 +1,12 @@
+days = "Mon Tue Wed Thurs Fri Sat Sun"
+months = "Jan\nfeb\nmarch\napril\nmay\njun\njuly\naug"
+
+print("Here are the days:",days)
+print("Here are the months:",months)
+
+print("""
+there is something going on here
+with three double qoutes.
+we will be able to type as much as we like.
+Even 4 lines if we want or 5 or 6.
+""" )
