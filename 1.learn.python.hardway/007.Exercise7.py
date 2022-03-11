4abef19344b638a1a04e30d13cd8eac3e95ae483
Vibha-Panwar <vibha9774@gmail.com>
2022-03-11 08:22:39 +0530

    11-march-2022

---
+++ 1.learn.python.hardway/007.Exercise7.py
@@ -0,0 +1,26 @@
+from ctypes.wintypes import HACCEL
+from re import A, I, T, U
+from tkinter import E, N, Y
+
+
+print("Mary has a little lumb.")
+print("It's fleece is white as {}.".format('snow'))
+print("and goes everywhere Mary goes.")
+
+print("@ _ @ " * 5)
+
+end1 = 1
+end2 = 2
+end3 = 3
+end4 = 4
+end5 = 5
+end6 = 6
+end7 = 7
+end8 = 8
+end9 = 9
+end10 = 10
+end11 = 11
+end = ''
+
+print(end1+end2+end3,end=',')
+print(end4+end5+end6+end7+end8+end9+end10+end11)
