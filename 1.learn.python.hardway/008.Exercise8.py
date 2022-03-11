44c9fddc87752a8572cd57d1fca10d3451313e25
Vibha-Panwar <vibha9774@gmail.com>
2022-03-11 08:38:27 +0530

    11-march-2022

---
+++ 1.learn.python.hardway/008.Exercise8.py
@@ -0,0 +1,12 @@
+panwar = "{} {} {} {}"
+
+print(panwar.format(1,2,3,4))
+print(panwar.format("one","two","three","four"))
+print(panwar.format("true","false","true","false"))
+print(panwar.format(panwar, panwar, panwar, panwar))
+print(panwar.format(
+    "kola-jinni",
+    "shocki-chimmy",
+    "taekook",
+    "jhope"
+))
