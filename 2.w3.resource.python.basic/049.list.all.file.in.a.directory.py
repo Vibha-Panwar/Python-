b9fefcdb55621e60d8dff59b01cb9733ad23aa01
Vibha-Panwar <vibha9774@gmail.com>
2022-03-11 11:03:28 +0530

    11-march-2022 {updated}

--- 2.w3.resource.python.basic/049.list.all.file.in.a.directory.py
+++ 2.w3.resource.python.basic/049.list.all.file.in.a.directory.py
@@ -3,5 +3,5 @@
 
 from os import listdir
 from os.path import isfile,join
-files_list = [f for f in listdir('/home/students') if isfile(join('/home/students',f)) ]
+files_list = [f for f in listdir('\..\..') if isfile(join('\..\..',f)) ]
 print(files_list);
