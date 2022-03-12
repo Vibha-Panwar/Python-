89a46d2fbab4d610efe5001db279d3bcd5c61e73
Vibha-Panwar <vibha9774@gmail.com>
2022-03-12 07:27:36 +0530

    12-march-2022

--- 1.learn.python.hardway/010.Execise10.py
+++ 1.learn.python.hardway/010.Execise10.py
@@ -1,13 +1,13 @@
 Tabby_cat = "\tI am tabbed in."
 Persian_cat = "I am split\non a line."
-Backslash_cat = "I am \\ a \\ cat."
+Backslash_cat = "I am \ a \ cat."
 
-Fat_cat = """
+Fat_cat = '''
 I'll do a list:
 \t* cat food
 \t* Fishies
 \t* catnip\n\t* Grass
-"""
+'''
 
 print(Tabby_cat)
 print(Persian_cat)
