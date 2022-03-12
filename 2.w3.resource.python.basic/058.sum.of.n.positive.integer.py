b0de7c9d9c5a24054a359383ef6d7282042cd667
Vibha-Panwar <vibha9774@gmail.com>
2022-03-12 08:10:52 +0530

    12-march-2022

---
+++ 2.w3.resource.python.basic/058.sum.of.n.positive.integer.py
@@ -0,0 +1,6 @@
+# https://www.w3resource.com/python-exercises/python-basic-exercise-58.php
+# Write a Python program to sum of the first n positive integers.
+
+n = int(input("Input a number :"))
+sum_num = (n*(n+1))/2
+print("Sum of the first",n,"positive integer:",sum_num)
