3fe61e5f81042afd15cdd59e4892cf02715fb673
Vibha-Panwar <vibha9774@gmail.com>
2022-03-12 07:42:22 +0530

    12-march-2022

---
+++ 1.learn.python.hardway/011.Exercise11.py
@@ -0,0 +1,8 @@
+print("How old are you?")
+age = input()
+print("How tall are you?")
+height = input()
+print("How much do you weight?")
+weight = input()
+
+print(f"So, You are {age} old,{height} tall and {weight} heavy.")
