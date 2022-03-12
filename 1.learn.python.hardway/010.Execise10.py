3be7ae7e30d0b80aa384fa57ae64449eb43aecb0
Vibha-Panwar <vibha9774@gmail.com>
2022-03-12 07:06:20 +0530

    12-march-2022

---
+++ 1.learn.python.hardway/010.Execise10.py
@@ -0,0 +1,15 @@
+Tabby_cat = "\tI am tabbed in."
+Persian_cat = "I am split\non a line."
+Backslash_cat = "I am \\ a \\ cat."
+
+Fat_cat = """
+I'll do a list:
+\t* cat food
+\t* Fishies
+\t* catnip\n\t* Grass
+"""
+
+print(Tabby_cat)
+print(Persian_cat)
+print(Backslash_cat)
+print(Fat_cat)
