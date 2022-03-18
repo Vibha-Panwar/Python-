d0d2a1f58fd26405fe993b73cbcc5520ab58f04c
Vibha-Panwar <vibha9774@gmail.com>
2022-03-18 11:55:33 +0530

    18-march-2022

--- 1.learn.python.hardway/015.Exercise15.py
+++ 1.learn.python.hardway/015.Exercise15.py
@@ -6,7 +6,7 @@ txt = open(filename)
 print(f"Here's your file {filename} :")
 print(txt.read())
 
-print("Type thefile name again :")
+print("Type the file name again :")
 file_again = input(">")
 
 txt_again = open(file_again)
