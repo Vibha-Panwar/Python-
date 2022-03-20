0512e398aa938c6649cf11a0ecfd0e3f520ca21c
Vibha-Panwar <vibha9774@gmail.com>
2022-03-20 09:06:01 +0530

    20-march-2022(2nd try & shorten it.)

--- 1.learn.python.hardway/017.Exercise17.py
+++ 1.learn.python.hardway/017.Exercise17.py
@@ -7,15 +7,10 @@ print(f"Copying from {from_file} to {to_file} .")
 vibha1 = open(from_file)
 vibha2 = vibha1.read()
 
-print(f"The input file is {len(vibha2)} bytes long.")
 print(f"Does the output file exist? {exists(to_file)}")
-print("Read, hit the RETURN to continue,CTRL-C to abort.")
-input()
 
 vibha3 = open(to_file , 'w')
 vibha3.write(vibha2)
-
-print("Alright, All Done!!")
 
 vibha3.close()
 vibha1.close()
