6d53c3c205fc3682ce34359f7c2f786223ea627c
Vibha-Panwar <vibha9774@gmail.com>
2022-03-30 07:32:28 +0530

    30-march-2022

---
+++ 1.learn.python.hardway/029.Exercise29.py
@@ -0,0 +1,26 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+    print("Too many cats , We are doomed!")
+
+if people > cats:
+    print("Not many cats , We are saved!")
+
+if people < dogs:
+    print("The world is drooled on!")
+
+if people > dogs:
+    print("The world is dry.")
+
+dogs += 5
+
+if people >= dogs:
+    print("people are greater then or equal to dogs.")
+
+if people <= dogs:
+    print("people are less then or equal to dogs.")
+
+if people == dogs:
+    print("people are dogs.")
