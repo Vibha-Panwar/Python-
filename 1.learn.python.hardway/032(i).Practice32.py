3deeeb74d4b0dd6b33dc3acebfc3ddbdc7d3f4de
Vibha-Panwar <vibha9774@gmail.com>
2022-04-03 14:31:19 +0530

    3-april-2022

---
+++ 1.learn.python.hardway/032(i).Practice32.py
@@ -0,0 +1,8 @@
+home = ['grandmaa','papa','maa','uncles' ]
+animals = ['golo','bhuro','maato']
+
+for name in home:
+    print(f"{name} is elder in my family.")
+
+for animal in animals:
+    print(f"{animal} is our heart. we love her so much.")
