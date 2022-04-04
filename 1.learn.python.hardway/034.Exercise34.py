f8c06926ed72e36e0f33f90b7a6b9aa1b004e3cc
Vibha-Panwar <vibha9774@gmail.com>
2022-04-04 22:55:07 +0530

    4-april-2022

---
+++ 1.learn.python.hardway/034.Exercise34.py
@@ -0,0 +1,9 @@
+animals = ['bear','peacock','kangaroo','whale','elephant']
+list2 = ['kola','alpaca','kitty','horse','duck','tiger','rabbit']
+
+print("animals[0]:",animals[0])
+print("list2[5]:",list2[5])
+
+print(f"I love list2 members {list2[5]}")
+tk = animals[0] + list2[6]
+print(tk)
