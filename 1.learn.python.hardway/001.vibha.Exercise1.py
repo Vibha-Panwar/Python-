e1f1b05955856f2ca04a1bb6232dd84bbec1b1a9
Vibha-Panwar <vibha9774@gmail.com>
2022-04-11 06:56:05 +0530

    11-april-2022

---
+++ 1.learn.python.hardway/001.vibha.Exercise1.py
@@ -0,0 +1,14 @@
+# I am starting to do this exercise again.
+# so that I can understand the function, whichever is used in the exercise, more.
+# And i don't have any kind of confusion. 
+
+
+print("!!! Heat Waves !!!")
+print("Sometimes all I think about is You.")
+print("Late nights in the middle of June.")
+print("Heat waves been fricking me out.")
+print("can't make you happier now.")
+print("I don't wanna be alone.")
+print("You know it hurts me too.")
+print("You looks so broken when you cry.")
+print("One more then I say Goodbye.")
