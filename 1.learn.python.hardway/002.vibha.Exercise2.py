0b28112251b0defc0d343b1c9122086e402f1819
Vibha-Panwar <vibha9774@gmail.com>
2022-04-12 06:54:11 +0530

    12-april-2022

---
+++ 1.learn.python.hardway/002.vibha.Exercise2.py
@@ -0,0 +1,12 @@
+print("Last exercise we get to know about the print function.")
+print("In this exercise, We will learn how we can make python to not read/process on what we right in the program.")
+# this will be ignored by python.
+
+print("Did you get what I just do???")
+print("No!! , Then have concentration when I do it again.")
+
+# Can you see it now.
+# I am doing this magic now.
+
+print("Plz tell me that you understand it.")
+print("Yeee!! that's right. If we put Hase sigh (#) infront of any line then python will ignore it.")
