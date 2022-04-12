00f4af3be35fd18a9ce5d8b10cc89a0046117e80
Vibha-Panwar <vibha9774@gmail.com>
2022-04-12 06:06:56 +0530

    12-april-2022

--- 4.Python.Puzzles/04.Exercise4.py
+++ 4.Python.Puzzles/04.Exercise4.py
@@ -2,7 +2,8 @@
 # We are making n stone piles! The first pile has n stones. If n is even, then all piles have an even number of stones. 
 # If n is odd, all piles have an odd number of stones.Each pile must more stones than the previous pile but as few as possible. 
 # Write a Python program to find the number of stones in each pile.
-
+# I need to do some changes.
+# just to understand it better.
 def test(n):
     return [n+2 * i for i in range (n)]
 
