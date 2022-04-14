1c5f1f3baed9a2af0ebd316fa0dcac4a029ea4d2
Vibha-Panwar <vibha9774@gmail.com>
2022-04-14 07:21:58 +0530

    14-april-2022

---
+++ 1.learn.python.hardway/004.vibha.Exercise4.py
@@ -0,0 +1,10 @@
+# In this exercise we will give value to some veriables and do some math afterword with the help of veriables.
+movie_halls = 100
+halls_capacity = 300
+customer = 200
+ticket_booked = customer
+ticket_not_booked = halls_capacity - customer
+market_capacity = movie_halls * customer
+
+print(f"There are{movie_halls} halls in this market.")
+print(f"There are {ticket_booked} booked in each movie_halls.")
