57fc8f3c8269fcc2c9e1176a70085a448493460e
Vibha-Panwar <vibha9774@gmail.com>
2022-04-15 06:21:27 +0530

    15-april-2022

---
+++ 1.learn.python.hardway/005.vibha.Exercise5.py
@@ -0,0 +1,12 @@
+# In this exercise we try out f-string function which we used in last program without knowing.
+# that's a mistake of mine. No problem !! we will use this function here again.
+
+print("here, I will talk about my mom's weight and hight.")
+Height = 165 # centimeter
+Weight = 80 # kilogram
+
+Change_Height = Height / 100 # in meter
+Change_Weight = Weight * 2.2 # in LB
+
+print(f"First we have Height {Height} in centimeter and weight {Weight} in kilogram.")
+print(f"Then we changed the value of height {Change_Height} in meter and weight {Change_Weight} in LBs")
