f2656ec7aeca6c7291638589508bc7356d5496a7
Vibha-Panwar <vibha9774@gmail.com>
2022-04-16 21:25:52 +0530

    16-april-2022

---
+++ 1.learn.python.hardway/006.vibha.Exercise6.py
@@ -0,0 +1,22 @@
+types_of_people = 2
+x = f'There are {types_of_people} types of people in the world.'
+
+BTS_army = 'BTS-lover'
+BTS_hater = 'loser'
+y = f'Those who {BTS_army} love themselves and those who {BTS_hater} hate everyone including themselves.'
+
+print(x)
+print(y)
+
+print(f"I said:{x}")
+print(f"I also said:{y}")
+
+Hilarious = True
+Joke_evaluation = 'Is not GRAMMY doing bias thing while giving award?? and neglect BTS completely.{}'
+
+print(Joke_evaluation.format(Hilarious))
+
+
+w = "This is the left side of the string"
+e = "\nwhile the other one is here. "
+print(w+e)
