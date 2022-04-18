eafcffc1712e5f07a8353493dbd2057d7a6b24db
Vibha-Panwar <vibha9774@gmail.com>
2022-04-18 09:22:40 +0530

    18-april-2022

---
+++ 1.learn.python.hardway/011.vibha.Exercise11.py
@@ -0,0 +1,10 @@
+# In this exercise, we will get to know about the input function.
+
+print("Do you know BTS??")
+Other_person1 = input()
+print("Did you attend their latest PTD concert in las vegus??")
+Other_person2 = input()
+print("Are you a Korean??")
+Other_person3 = input()
+
+print(f"So you {Other_person1} know BTS and {Other_person2} attend the concert and you are{Other_person3}.")
