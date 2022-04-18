b68d007799ce2697dc807e5237cd1f2a16f42e98
Vibha-Panwar <vibha9774@gmail.com>
2022-04-18 06:15:50 +0530

    18-april-2022

---
+++ 1.learn.python.hardway/009.vibha.Exercise9.py
@@ -0,0 +1,17 @@
+# In this we learn just 1 new thing and that is how can we give many line long statement in one print function.
+
+Family = "Grandmaa,Father,Mother,Uncles,Oldest brother,Sister-in-law,Middle brother,and then me(youngest)"
+print("Here is my family member:",Family)
+
+print("""
+Can we give the names of the Family now::
+Grandmaa -- Chandrawati
+Father -- Satish Panwar
+Mother -- Geeta Panwar
+Uncles --(i) Sudhir Panwar
+         (ii) Praveen Panwar
+Oldest Brother -- Vishvendra Panwar
+Sister-in-law -- Monika Panwar
+Middle Brother -- Aaditya Panwar
+Me -- Vibha Panwar 
+""")
