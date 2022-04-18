7d45632acfab6bdbccf63874ce0ce62f31f2f89e
Vibha-Panwar <vibha9774@gmail.com>
2022-04-18 06:55:11 +0530

    18-april-2022

---
+++ 1.learn.python.hardway/010.vibha.Exercise10.py
@@ -0,0 +1,28 @@
+# In this program , We will notice some symboles and try to figure them out what they do.
+# just give it a shot.
+
+Black_cat = "\tIt is Suga's fan."
+Golden_cat = "\n\tIt is Jimin's fan."
+Tata = "It represent \ Taehyung \ in B12 World."
+
+Cola = """
+It is the leader of the group So it isits duty to allot work to everyone.
+To Do List:-
+\t* Write a post on my blog.
+\t* Do 3 Python Program
+\t* attend the Class
+\t* Revise today's class\n\t* Do some yoga & exercise
+ """
+
+print(Black_cat)
+print(Golden_cat)
+print(Tata)
+print(Cola)
+
+# Did you notice what happen.
+# Okay!! I Explain it now.
+# \t -- a Tab ,it mean it will give a big space in one go which is equal to the space of 5 spacebar space.
+# \n -- it will write whatever we write  after it in 'Next Line'.
+# \ -- It is simple .You have also noticed it already that it run as is it.
+# So by these little letter and symboles we can escape from entering 'Enter' and write every same thing again in next line.
+# Simple !!!! Isn't it??
