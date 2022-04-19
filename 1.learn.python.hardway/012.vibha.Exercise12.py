01c7411f56c0a086197eb7b040fa0b93ff75f5b8
Vibha-Panwar <vibha9774@gmail.com>
2022-04-19 06:40:03 +0530

    19-april-2022

---
+++ 1.learn.python.hardway/012.vibha.Exercise12.py
@@ -0,0 +1,8 @@
+# In this exercise we will try to shorten the previous program.
+
+Family = input("how many family members are in your family?")
+City = input("In which city do you live?")
+
+print(f"So you have{Family} members in your family and you live in {City}.")
+
+# So, here we defined a variable and ask for input from the user in the same line.
