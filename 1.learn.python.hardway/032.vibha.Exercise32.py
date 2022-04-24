061c71e50ed6d1640e9c1614be89e6bdf0f3e8ea
Vibha-Panwar <vibha9774@gmail.com>
2022-04-24 17:56:24 +0530

    24-april-2022

---
+++ 1.learn.python.hardway/032.vibha.Exercise32.py
@@ -0,0 +1,21 @@
+numbers = [1,2,3,4,5,6,7,8,9]
+Names = ['Vibha','Varsha','Jyoti']
+Nike_Names = ['Chimmy','Sunshine','Taetae','Koo','W.W.W.','Namjoonie','Savage-King']
+
+for number in numbers:
+    print(f"The number is {numbers}")
+
+for Name in Names:
+    print(f"The name is {Names}")
+
+for Nike_Name in Nike_Names:
+    print(f"The Nike_Name is {Nike_Names}")
+
+Element = []
+for i in range(0,9):
+    print(f"Adding {i} to the list")
+    Element.append(i)
+
+for i in Element:
+    print(f"Element is:{i}")
+    print(Element)
