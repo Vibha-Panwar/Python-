d1fc02be1d98ab07e72ecf7aa6c8af2786e06685
Vibha-Panwar <vibha9774@gmail.com>
2022-04-24 17:03:15 +0530

    24-april-2022

--- 1.learn.python.hardway/033.vibha(i).Exercise33.py
+++ 1.learn.python.hardway/033.vibha(i).Exercise33.py
@@ -5,3 +5,18 @@ count = 0
 while count < 5:
     count += 1
     print(" Vibha,You are Beautifull !!!")
+
+X = 2
+Counting = []
+
+while X < 10:
+    print(f"count on the top is {X}")
+    Counting.append(X)
+    X += 1
+    print("Counting now",Counting)
+    print(f"At the bottem Counting is {X}")
+
+print("The Counting is:")
+for Count in Counting:
+    print(Counting)
+
