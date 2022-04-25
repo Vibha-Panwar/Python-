09e458d281f88bdeff2acf0e59cb6a731e1f1845
Vibha-Panwar <vibha9774@gmail.com>
2022-04-25 07:02:59 +0530

    25-april-2022

---
+++ 1.learn.python.hardway/029.vibha.Exercise29.py
@@ -0,0 +1,22 @@
+people = 20
+germon_sefored = 15
+huskey = 25
+
+if people < germon_sefored:
+    print("people like germon_sefored now a days.")
+else:
+    print("there are still so many people who don't like dogs of any bread.")
+
+if people < huskey:
+    print("huskey bread of dogs is getting popular here.")
+else:
+    print("people afraid of huskey.")
+
+
+germon_sefored += 10
+if people <= germon_sefored:
+    print("people like to have more than 1 germon-sefored with them.")
+elif people >= germon_sefored:
+    print("people start liking the germon_sefored with a great speed.")
+else:
+    print("I don't know what to write here.")
