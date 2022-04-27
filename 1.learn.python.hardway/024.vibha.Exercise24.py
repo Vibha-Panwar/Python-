d2ff00dd1053acf07657ee9aed4c936a43bfadbe
Vibha-Panwar <vibha9774@gmail.com>
2022-04-27 09:39:24 +0530

    27-april-2022

---
+++ 1.learn.python.hardway/024.vibha.Exercise24.py
@@ -0,0 +1,19 @@
+print("It says we will do everything.")
+print("Whatever we have done in previous exercises.")
+print("In this single exercise")
+print("First start with symboles that repersent\ nothing.\\ with this also.")
+print("but if we give \n this then next line will be start and if we give \t then we find out there will be a little more Gap then usual.")
+
+poem = ("""
+\t I wanna be with you
+   And I wanna stay with you
+  Just like the stars shining bright
+\t You're glowing once more
+""")
+print("""---------""")
+print(poem)
+print("***-----***")
+
+multple_of_five = 50*2/5+10
+print(f"This sould be thirty {multple_of_five}.")
+
