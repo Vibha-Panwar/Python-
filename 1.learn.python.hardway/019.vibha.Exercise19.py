f593bfdfb6808ec6f06b0bbebcd5c445970476f3
Vibha-Panwar <vibha9774@gmail.com>
2022-04-27 08:03:32 +0530

    27-april-2022

---
+++ 1.learn.python.hardway/019.vibha.Exercise19.py
@@ -0,0 +1,20 @@
+def Dairy_Milk_and_Milky_Bar(No_of_Dairy_Milk,Bars_of_Milky_Bar):
+    print(f"We have {No_of_Dairy_Milk} Dairy_Milk Chocolates.")
+    print(f"We have {Bars_of_Milky_Bar} Milky_Bar Chocolates.")
+
+Dairy_Milk_and_Milky_Bar(10,15)
+
+# We can give the input in other way too.
+
+Dairy_Milk = 20
+Milky_Bar = 25
+print(f"We will distribute {Dairy_Milk} Dairy_Milk Chocolates and {Milky_Bar} Milky_Bar Chocolates in the class.")
+
+# we can do little math exercise in it too.
+
+Dairy_Milk_and_Milky_Bar(10*5,100/5)
+print("This is what we get after do math inside it.")
+
+print("We can combine two veriable too.")
+Dairy_Milk_and_Milky_Bar(Dairy_Milk+2 , Milky_Bar-5)
+Dairy_Milk_and_Milky_Bar(Milky_Bar+Dairy_Milk , Milky_Bar*5)
