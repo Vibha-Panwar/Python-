e0b556c491d2c8b52a589de1f608585ec946e30e
Vibha-Panwar <vibha9774@gmail.com>
2022-04-27 08:40:26 +0530

    27-april-2022

---
+++ 1.learn.python.hardway/021.vibha.Exercise21.py
@@ -0,0 +1,26 @@
+def add(a,b):
+    print(f"Adding {a} + {b}")
+    return a+b
+
+def subtract(a,b):
+    print(f"Subtract {a} - {b}")
+    return a-b
+
+def multiply(a,b):
+    print(f"Multiplying {a}*{b}")
+    return a*b
+
+def module(a,b):
+    print(f"Module {a}%{b}")
+    return a%b
+
+print("Let's do some math with the functions we defined above.")
+Age = add(15,10)
+weight = subtract(85,10)
+height = multiply(83,2)
+IQ = module(200,3)
+print(f"Age: {Age},weight: {weight}, height: {height} and IQ: {IQ}")
+
+puzzle = add(Age,subtract(weight,multiply(height,module(IQ,2))))
+print("Can you solve it by hand.")
+print(puzzle)
