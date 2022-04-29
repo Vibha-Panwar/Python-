24988a63fd300c698ce0d0bebdf54b38712e462c
Vibha-Panwar <vibha9774@gmail.com>
2022-04-29 07:20:16 +0530

    29-april-2022

--- 1.learn.python.hardway/024.vibha.Exercise24.py
+++ 1.learn.python.hardway/024.vibha.Exercise24.py
@@ -17,3 +17,19 @@ print("***-----***")
 multple_of_five = 50*2/5+10
 print(f"This sould be thirty {multple_of_five}.")
 
+def secret_formula(starting_point):
+  Jelly_beans = start_point * 500
+  Jars = Jelly_beans /100
+  cracker = Jars + 100
+  return Jelly_beans,Jars,cracker
+
+start_point = 10000
+beans,Jars,cracker = secret_formula(start_point)
+
+print("with a starting point of: {}".format(start_point))
+print(f"We have {Jelly_beans} beans , {Jars} Jars and {cracker}")
+
+start_point = start_point /10
+print("Can we do it in another way?")
+formula = secret_formula(start_point)
+print("we'd have beans {} and {} Jars and {} crackers.".formula(*formula))
