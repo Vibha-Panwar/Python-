39d73bde8f157168e9d5057b23d264e400867939
Vibha-Panwar <vibha9774@gmail.com>
2022-08-09 20:05:02 +0530

    09-august-2022

---
+++ 6.1000.Python.Example/001.Hello.py
@@ -0,0 +1,62 @@
+print("Hello World!!!")
+# This is a comment.
+print("I am a human.") # This is not going to print.
+# print("This is also not going to print in the output.")
+greeting = "Good Evening"
+print(greeting)
+print(type(12)) # Integer
+print(type(1.2)) # Floating
+print(type("string")) # String
+print(type("23")) # String
+print(type("2.34")) # String
+print(type(None))
+print(type(True)) # Boolean
+print(type(False)) # Boolean
+print(type([])) # List
+print(type({})) # dictionary
+print(type("hello"))
+print("still running")
+print(0.1 + 0.2)
+# Numerical Operations:-
+# Multiply Numbers
+width = 25
+height = 15
+area = width*height
+print(area)
+# Add Number
+a = 12
+b = 13
+addition = a+b
+print(addition)
+# Multiply String
+#width = "10"
+#height = "15"
+#area = width * height
+#print(area)
+# Add String
+a = "10"
+b = "12"
+addition = a+b
+print(addition)
+# HomeWork
+width = 10
+height = 20
+area = width * height
+print(area)
+perimeter = 2*(width + height)
+print(perimeter)
+# 2. homework
+r = 7
+pi = 3.14
+area = pi * r * r
+print(area)
+circumference = 2*pi*r
+print(circumference)
+# 3. Homework
+a = 12
+b = 4
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+print (c,d,e,f)
