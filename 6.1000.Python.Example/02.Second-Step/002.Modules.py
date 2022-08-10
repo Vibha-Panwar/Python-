c4177226223977872e958249af1b341ff90f60fc
Vibha-Panwar <vibha9774@gmail.com>
2022-08-10 11:43:40 +0530

    10-august-2022

---
+++ 6.1000.Python.Example/02.Second-Step/002.Modules.py
@@ -0,0 +1,63 @@
+import sys
+print(sys.executable)
+print(sys.platform)
+print(sys.argv[0])
+print(sys.version_info.major)
+
+print(sys.getsizeof(2))
+print(sys.getsizeof(50))
+print(sys.getsizeof(3.5))
+
+print(sys.getsizeof(""))
+print(sys.getsizeof("z"))
+print(sys.getsizeof("hello"))
+print(sys.getsizeof("hello world"))
+
+# A Main Function
+def main():
+    print("Hello")
+    print("World")
+print("Before")
+main()
+print("After")
+
+# Print Function in Python3
+print("Hello",end=" ")
+print("World")
+print("Foo","Bar")
+# end will set the next character added at the end of each print statement.
+
+print("Hello",end=" ")
+print("World")
+print("Foo","Bar","Moon",sep=" ")
+print("Sun")
+
+# Prompting for user input in Python3
+def main():
+    print("We have a question?")
+    name = input("your name : ")
+    print("hello" + name + ",How are you ?")
+main()
+# Add number enter by the user
+def main():
+    a = input("First Number : ")
+    b = input ("Second Number : ")
+    print(a + b)
+main()
+# Input function take value as a string. No matter what, You give value as a number or words, 
+# Addition (+) function will add those value as string addition. ex- a=1,b=2,a+b = 12
+
+# Add number enter by user (fixed)
+def main():
+    a = input("First Number : ")
+    b = input("Second Number : ")
+    print(int(a)+int(b))
+main()
+# How can I check If a string can be converted to a number ?
+val = input("Type in a number : ")
+print(val)
+print(val.isdecimal())
+print(val.isnumeric())
+if val.isdecimal():
+    num = int(val)
+    print(num)
