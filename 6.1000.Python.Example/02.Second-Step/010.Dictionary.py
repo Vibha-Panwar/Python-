0c2020fbc9e0338d3a21d7c4b1d186b68088eb05
Vibha-Panwar <vibha9774@gmail.com>
2022-09-12 18:26:26 +0530

    12-september-2022

--- 6.1000.Python.Example/02.Second-Step/010.Dictionary.py
+++ 6.1000.Python.Example/02.Second-Step/010.Dictionary.py
@@ -183,3 +183,14 @@ number = {
 print(number)
 print(number[17])
 print(number.values( ))
+
+# immutable collection : Tuple as dictionary Key
+points = {}
+p1 = (2,3)
+points[p1] = 'Joe'
+points[(17, 5)] = 'Jane'
+print(points)
+for k in points.keys():
+    print(k)
+    print(k.__class__.__name__)
+    print(points[k])
