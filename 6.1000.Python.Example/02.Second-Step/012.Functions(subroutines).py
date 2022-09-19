8b395ee2fe36145de222b9c813c8feac934b1e80
Vibha-Panwar <vibha9774@gmail.com>
2022-09-19 10:48:32 +0530

    19-september-2022

--- 6.1000.Python.Example/02.Second-Step/012.Functions(subroutines).py
+++ 6.1000.Python.Example/02.Second-Step/012.Functions(subroutines).py
@@ -4,7 +4,7 @@
 # closed parentheses is followed by a colon ":".Then the body of the function is indented to the right.
 # The depth of indentation does not matter but it must be the same for all the lines of the function.
 # When we stop the indentation and start a new expression on the first column, that’s what tells Python that the function defintion has ended.
-
+"""
 def add(x, y):
     z= x + y
     return z
@@ -23,3 +23,45 @@ def sendmail(From, To, Subject, Content):
     print(Content)
     
 sendmail('abc','def', 'xyz', 'lmnop')
+
+## Parameter can be named :-->
+def sendmail(From, To, Subject, Content):
+    print('From :', From)
+    print('To :', To)
+    print('Subject :', Subject)
+    print('')
+    print(Content)
+
+sendmail(
+    Subject = 'self message',
+    Content = 'Has some content too',
+    From = 'gaasdf',
+    To = 'fdfgafg',
+)
+"""
+"""
+## Mixed positional and named parameter :--> We can not mix them, choose either one.
+def sendmail(From, To, Subject, Content):
+    print('From', From)
+    print('To', To)
+    print('Subject', Subject)
+    print('')
+    print(Content)
+
+sendmail(
+    Subject = 'dfgadaf',
+    Content = 'bdrytjry',
+    To = 'sdgh.com',
+    'sdfsgdgg.com',
+)
+"""
+## Default Values :-->
+def prompt(question, retry=3):
+    while retry > 0:
+        inp = input('{} ({}) :'.format(question, retry))
+        if inp == 'my secret':
+            return True
+        retry -= 1
+    return False
+print(prompt("Type in your password"))
+print(prompt("Type in your password", 1))
