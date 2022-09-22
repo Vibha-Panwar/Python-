d231c82edb8411147e61fff52d421b240ffca456
Vibha-Panwar <vibha9774@gmail.com>
2022-09-22 21:52:13 +0530

    22-september-2022

---
+++ 1.learn.python.hardway/003.MH.Exercise3.py
@@ -0,0 +1,11 @@
+# In this exercise , we have to do some math.
+# Let see what comes to my mind for this exercise.
+
+
+print("i will count my shoping bills:")
+print("In VS Mall",1000+2500-500+800/40)
+print("In World of Wonder",2300+100%4*3)
+
+print("Now I will count my travelling expencises.")
+
+print(2000+300-500*3+200/2)
