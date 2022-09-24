d04ce3bc7a67dd6dbef8d7b511a6157c86f2e08b
Vibha-Panwar <vibha9774@gmail.com>
2022-09-24 14:11:05 +0530

    24-september-2022

---
+++ 6.1000.Python.Example/02.Second-Step/012.1.Functions.py
@@ -0,0 +1,19 @@
+# Copy-Paste Code :-->
+a = [2, 3, 93, 18]
+b = [27, 81, 11, 35]
+c = [32, 103, 11]
+
+total_a = 0
+for v in a:
+    total_a += v
+print("sum of a : {} average of a : {}".format(total_a, total_a / len(a)))
+
+total_b = 0
+for v in b:
+    total_b += v
+print("sum of b : {} average of b : {}".format(total_b, total_b / len(b)))
+
+total_c = 0
+for v in c:
+    total_c += v
+print("sum of c : {} average of c : {}".format(total_c, total_c / len(c)))
