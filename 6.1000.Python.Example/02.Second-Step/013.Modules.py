f8b7fb13026e0057edac721e3893d7e7fdd76365
Vibha-Panwar <vibha9774@gmail.com>
2022-09-26 13:45:49 +0530

    26-september-2022

---
+++ 6.1000.Python.Example/02.Second-Step/013.Modules.py
@@ -0,0 +1,5 @@
+### Modules :--> Module is just a Python file with a set of functions. It is same as code liberary.
+# modules contain function but it can also contain variable of all type like- array, Dictionary etc.
+def add(a, b):
+    return a+b
+
