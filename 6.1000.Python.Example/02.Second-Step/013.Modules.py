157259fd124aed423665d2e939d132556719d776
Vibha-Panwar <vibha9774@gmail.com>
2022-10-07 11:30:54 +0530

    7-october-2022

--- 6.1000.Python.Example/02.Second-Step/013.Modules.py
+++ 6.1000.Python.Example/02.Second-Step/013.Modules.py
@@ -1,7 +1,28 @@
 ### Modules :--> Module is just a Python file with a set of functions. It is same as code liberary.
 # modules contain function but it can also contain variable of all type like- array, Dictionary etc.
+"""
 def add(a, b):
     return a+b
 z = add(2,3)
 print(z) 
 
+import my_calculator
+z = my_calculator.add(6,3)
+print(z)
+
+# A module is simply a Python file with a .py extension that can be imported inside another Python program.
+# The name of the Python file becomes the module name.
+# The module contains definitions and implementation of classes, variables, and functions that can be used inside another program.
+
+import my_calculator
+Q = my_calculator.sub(4,6)
+print(Q)
+
+from my_calculator import multi
+
+print(multi(4, 5))
+
+import sys
+print(sys.path)
+"""
+### Flat Project directory Structure:-->
---
+++ 6.1000.Python.Example/02.Second-Step/my_calculator.py
@@ -0,0 +1,14 @@
+def add(a, b):
+    return(a+b)
+
+def sub(a, b):
+    if a > b:
+      return(a-b)
+    else:
+        return(b-a)
+
+
+def multi(a, b):
+    return(a*b)
+
+
