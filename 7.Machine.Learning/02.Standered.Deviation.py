0f883eb60b890cb6ce29a9002ed99e756d8026fe
Vibha-Panwar <vibha9774@gmail.com>
2023-01-22 01:39:10 +0530

    22-Jan-2023

---
+++ 7.Machine.Learning/02.Standered.Deviation.py
@@ -0,0 +1,7 @@
+# Standard Deviation:--> It is a number that describes how spread out the values are.
+# A low Standard Deviation means- no. are close tp mean value.
+# A High Standard Deviation means- no. are spread out over a wider range.
+import numpy
+speed = [86,87,88,86,85,86,87]
+x = numpy.std(speed)
+print(x)
