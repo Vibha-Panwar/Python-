0db0180a432bbe4e709912641ffecb938e24cc53
Vibha-Panwar <vibha9774@gmail.com>
2023-01-26 12:07:26 +0530

    26-Jan-2023

---
+++ 8.Python.Tutorial.By.W3School/001.Variables.py
@@ -0,0 +1,26 @@
+# Variables: Variables are containers for storing data values.
+# A variable is created the moment you first assign a value to it.
+x = 5
+y = "John"
+print(x)
+print(y)
+
+#Casting: If you want to specify the data type of a veriable, this can be done with casting.
+x = str(3)
+y = int(3)
+z = float(8)
+print(x)
+print(y)
+print(z)
+
+# We can get to know the data type of a variable with the 'type()'.
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+# Case Sensitive : Variable names are case-sensitive.
+a = 3
+A = "Sally"
+print(a)
+print(A)
