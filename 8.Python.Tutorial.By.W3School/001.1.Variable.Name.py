a9060bf4fc97fbdf6f78d17a42f3f52e9c29e67e
Vibha-Panwar <vibha9774@gmail.com>
2023-01-26 12:26:59 +0530

    26-Jan-2023

---
+++ 8.Python.Tutorial.By.W3School/001.1.Variable.Name.py
@@ -0,0 +1,12 @@
+# Variable Namaes Rule: Name must start with a letter or the undersocre character
+#                     : Name can't be started with  a number
+#                     : Name can only contain alpha-Numeric character & underscores(A-Z,0-9,_)
+#                     : Names are case-sensitive
+
+# Illegal Variable name : '-' in the middle of the name
+#                       : space between the name. 
+
+# Multi Words Variable Names : 
+# 1] Camel Case : - myVariableName = "Jonny"
+# 2] Pascal Case : - MyVariableName = "Jonny"
+# 3] Snake Case : - my_variable_name = "Jonny"
