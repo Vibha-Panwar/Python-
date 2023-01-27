7fb64b299f7202e15e0c63fb3f3f2fb16eec5da9
Vibha-Panwar <vibha9774@gmail.com>
2023-01-27 23:08:43 +0530

    27-Jan_23

---
+++ 8.Python.Tutorial.By.W3School/001.2.Variable.Output.py
@@ -0,0 +1,13 @@
+# You can output multiple variable,seperated by comas, in one print function.
+x = "Python"
+y = "is"
+z = "Awesome"
+print(x,y,z)
+
+# You can also use + sigh to output multiple variable.
+print(x+y+z)
+
+# + operator also use in mathmatical function.
+x = 34
+y = 23
+print(x + y)
