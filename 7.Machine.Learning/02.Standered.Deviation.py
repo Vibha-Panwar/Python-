6d0b3dd1bc752ad16b1c5b63de2fca2bd95e691c
Vibha-Panwar <vibha9774@gmail.com>
2023-02-04 17:02:23 +0530

    04-Feb-2023

--- 7.Machine.Learning/02.Standered.Deviation.py
+++ 7.Machine.Learning/02.Standered.Deviation.py
@@ -5,3 +5,12 @@ import numpy
 speed = [86,87,88,86,85,86,87]
 x = numpy.std(speed)
 print(x)
+
+# Variance: it is another number that indicates how spread out the values are.
+# How to calculate it :-->
+# 1] Find the mean
+# 2] Find the difference from the mean for each value
+# 3] Do square value of the difference we took in step 2.
+# 4] Now find mean of the square value. you will get the Vaiance.
+
+# Standered Deviation :--> Square root of Variance
