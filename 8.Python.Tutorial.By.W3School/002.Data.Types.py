58d0ff008e0c2d66abb1910defeedd176d292c47
Vibha-Panwar <vibha9774@gmail.com>
2023-02-05 21:25:57 +0530

    05-Feb-2023

---
+++ 8.Python.Tutorial.By.W3School/002.Data.Types.py
@@ -0,0 +1,47 @@
+## Data Type is an important concept in any programming. Variable can store
+## any type of data. Python has following data type built-in by default.
+# Text Type : str {string}
+# Numeric Type : int, float, complex
+# Sequence Type : list, tuple, range
+# Mapping Type : dict {dictionary}
+# Set Type : set, frozenset
+# Boolean Type : bool
+# Binary Type : bytes, bytearrey, memoryview
+# None Type : Nonetype
+
+# String --
+print("Hello World.")
+
+# Integer --
+x = 20
+print(x)
+
+# Float --
+x = 20.7
+print(x)
+
+# Complex --
+x = 1j
+print(x)
+
+# List --
+x = [1,2,3,4,5,6,7]
+print(x)
+
+# Tuple --
+x = ("a","d","t","g","u",1,4,6,8,9)
+print(x)
+
+# range --
+x = range(9)
+print(x)
+
+# Dictionary --
+x = {"Name" : "Victor","Age" : 66}
+print(x)
+print(type(x))
+
+# Set --
+x = {"Apple","BMW","Cat"}
+print(x)
+print(type(x))
