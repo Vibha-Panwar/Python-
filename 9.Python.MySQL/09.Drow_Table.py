4a4028709ef19079b7f1409fb61e915b61a67b0b
Vibha-Panwar <vibha9774@gmail.com>
2023-05-28 00:01:54 +0530

    28-MAY-2023

---
+++ 9.Python.MySQL/09.Drow_Table.py
@@ -0,0 +1,29 @@
+## Drop Table :--> means delete complete tables in one go.
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "1q2w!Q@WVibha",
+    database = "vibha"
+)
+
+mycursor = mydb.cursor()
+sql = "Drop Table customers"
+mycursor.execute(sql)
+
+
+### Drop Table Only If Exits :-->
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "1q2w!Q@WVibha",
+    database = "vibha"
+)
+
+mycursor = mydb.cursor()
+sql ="DROP TABLE IF EXISTS customers"
+mycursor.execute(sql)
